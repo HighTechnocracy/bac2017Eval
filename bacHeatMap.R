66d705cedbf80d70db87c715f95d787465c9f84c
HighTechnocracy <jim@hightechnocracy.com>
2017-02-07 15:25:34 -0700

    added R file

---
+++ bacHeatMap.R
@@ -0,0 +1,8 @@
+setwd("~/R/bac2017Eval")
+
+## Read the Data
+data <- read.csv("~/R/bac2017Eval/data/heatMapData.csv")
+
+pdf(file = "~/R/bac2017Eval/visuals/2017heatMap_pre.pdf")
+## Heatmap Code
+dev.off()
